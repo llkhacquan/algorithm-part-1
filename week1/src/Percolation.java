de3be157a855feacb5e65ca46227bfb6232322ee
quannk-coccoc <ll.khac.quan@gmail.com>
2016-11-16 17:37:04 +0700

    Fix bugs

--- week1/src/Percolation.java
+++ week1/src/Percolation.java
@@ -2,93 +2,96 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int n;
-    private boolean[][] openned;
-    private WeightedQuickUnionUF uf;
+	private boolean[] opened;
+	private WeightedQuickUnionUF uf;
+	private WeightedQuickUnionUF uf2;
+	private final int TOP;
+	private final int BOTTOM;
+	private boolean percolates = false;
 
     public Percolation(int inputN) {
         if (inputN < 1)
             throw new IllegalArgumentException("N should be greater than 0");
         n = inputN;
-        openned = new boolean[n][];
-        for (int i = 0; i < n; i++) {
-            openned[i] = new boolean[n];
-            for (int j = 0; j < n; j++)
-                openned[i][j] = false;
-        }
+		opened = new boolean[n * n];
 
-        // initiate Union-Find
-        uf = new WeightedQuickUnionUF(n * n + 2);
-    }
+		TOP = n * n;
+		BOTTOM = TOP + 1;
+		// initiate Union-Find
+		uf = new WeightedQuickUnionUF(n * n + 2);
+		uf2 = new WeightedQuickUnionUF(n * n + 1);
+	}
 
-    private int twoDtoOneD(int i, int j) {
-        if (!isLegalPosition(i + 1, j + 1))
-            throw new IndexOutOfBoundsException();
-        return i * n + j;
-    }
+	/**
+	 * 1<=i,j<=n
+	 */
+	private int _2to1(int i, int j) {
+		assert isLegal(i, j);
+		return (i - 1) * n + j - 1;
+	}
 
     public void open(int i, int j) {
-        if (!isLegalPosition(i, j))
-            throw new IndexOutOfBoundsException();
+		if (!isLegal(i, j)) {
+			throw new IndexOutOfBoundsException();
+		}
 
-        if (isOpen(i, j))
-            return;
+		if (isOpen(i, j)) {
+			return;
+		}
+		opened[_2to1(i, j)] = true;
 
-        openned[i - 1][j - 1] = true;
-        if (isLegalPosition(i - 1, j)) {
-            if (openned[i - 2][j - 1]) {
-                uf.union(twoDtoOneD(i - 1, j - 1), twoDtoOneD(i - 2, j - 1));
-            }
-        }
+		if (isLegal(i - 1, j) && opened[_2to1(i - 1, j)]) {
+			uf.union(_2to1(i, j), _2to1(i - 1, j));
+			uf2.union(_2to1(i, j), _2to1(i - 1, j));
+		}
 
-        if (isLegalPosition(i + 1, j)) {
-            if (openned[i][j - 1]) {
-                uf.union(twoDtoOneD(i - 1, j - 1), twoDtoOneD(i, j - 1));
-            }
-        }
+		if (isLegal(i + 1, j) && opened[_2to1(i + 1, j)]) {
+			uf.union(_2to1(i, j), _2to1(i + 1, j));
+			uf2.union(_2to1(i, j), _2to1(i + 1, j));
+		}
 
-        if (isLegalPosition(i, j - 1)) {
-            if (openned[i - 1][j - 2]) {
-                uf.union(twoDtoOneD(i - 1, j - 1), twoDtoOneD(i - 1, j - 2));
-            }
-        }
+		if (isLegal(i, j - 1) && opened[_2to1(i, j - 1)]) {
+			uf.union(_2to1(i, j), _2to1(i, j - 1));
+			uf2.union(_2to1(i, j), _2to1(i, j - 1));
+		}
 
-        if (isLegalPosition(i, j + 1)) {
-            if (openned[i - 1][j]) {
-                uf.union(twoDtoOneD(i - 1, j - 1), twoDtoOneD(i - 1, j));
-            }
-        }
+		if (isLegal(i, j + 1) && opened[_2to1(i, j + 1)]) {
+			uf.union(_2to1(i, j), _2to1(i, j + 1));
+			uf2.union(_2to1(i, j), _2to1(i, j + 1));
+		}
 
-        if (i == 1) {
-            uf.union(twoDtoOneD(i - 1, j - 1), n * n);
-        }
-        if (i == n) {
-            uf.union(twoDtoOneD(i - 1, j - 1), n * n + 1);
-        }
-    }
+		if (i == 1) {
+			uf.union(_2to1(i, j), TOP);
+			uf2.union(_2to1(i, j), TOP);
+		}
+		if (i == n) {
+			uf.union(_2to1(i, j), BOTTOM);
+		}
+	}
 
     public boolean isOpen(int i, int j) {
-        if (!isLegalPosition(i, j))
-            throw new IndexOutOfBoundsException();
-        return openned[i - 1][j - 1];
-    }
+		if (!isLegal(i, j)) {
+			throw new IndexOutOfBoundsException();
+		}
+		return opened[_2to1(i, j)];
+	}
 
     public boolean isFull(int i, int j) {
-        if (!isLegalPosition(i, j))
-            throw new IndexOutOfBoundsException();
-        // is site (row i, column j) full?
-        return uf.connected(twoDtoOneD(i - 1, j - 1), n * n);
-    }
+		if (!isLegal(i, j)) {
+			throw new IndexOutOfBoundsException();
+		}
+		return uf2.connected(_2to1(i, j), TOP);
+	}
 
     public boolean percolates() {
-        // does the system percolate?
-        return uf.connected(n * n, n * n + 1);
-    }
+		return uf.connected(TOP, BOTTOM);
+	}
 
-    private boolean isLegalPosition(int i, int j) {
-        if (i < 1 || j < 1 || i > n || j > n)
-            return false;
-        else
-            return true;
-    }
+	/**
+	 * 1<=i, j<=n
+	 */
+	private boolean isLegal(int i, int j) {
+		return !(i < 1 || j < 1 || i > n || j > n);
+	}
 
 }
--- week1/src/PercolationStats.java
+++ week1/src/PercolationStats.java
@@ -2,69 +2,66 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private int n;
-    private int nTrials;
-    private double[] array;
-    private double mean;
-    private double stddev;
+	private final double mean;
+	private final double stddev;
+	private final double low, high;
 
-    public PercolationStats(int inputN, int trials) {
-        if (inputN < 1 || trials < 1)
-            throw new IllegalArgumentException();
-        n = inputN;
-        nTrials = trials;
+	public PercolationStats(int n, int trials) {
+		if (n < 1 || trials < 1) {
+			throw new IllegalArgumentException();
+		}
+		double[] array = new double[trials];
 
-        array = new double[nTrials];
+		for (int iTrial = 0; iTrial < trials; iTrial++) {
+			array[iTrial] = (double) testPercolation(n) / n / n;
+		}
+		mean = StdStats.mean(array);
+		stddev = StdStats.stddev(array);
+		low = mean - 1.96 * stddev / Math.sqrt(trials);
+		high = mean + 1.96 * stddev / Math.sqrt(trials);
+	}
 
-        for (int iTrial = 0; iTrial < nTrials; iTrial++) {
-            array[iTrial] = (double) testPercolation(inputN) / inputN / inputN;
-        }
-        mean = StdStats.mean(array);
-        stddev = StdStats.stddev(array);
-    }
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int nTrials = Integer.parseInt(args[1]);
+		PercolationStats starts = new PercolationStats(n, nTrials);
+		System.out.println("mean                    = " + starts.mean());
+		System.out.println("stddev                  = " + starts.stddev());
+		System.out.println("95% confidence interval = " + starts.confidenceLo() + ", " + starts.confidenceHi());
+	}
 
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        int nTrials = Integer.parseInt(args[1]);
-        // System.out.println("Start running");
-        PercolationStats starts = new PercolationStats(n, nTrials);
-        System.out.println("mean                    = " + starts.mean());
-        System.out.println("stddev                  = " + starts.stddev());
-        System.out.println("95% confidence interval = " + starts.confidenceLo() + ", " + starts.confidenceHi());
-    }
+	private static int testPercolation(int n) {
+		Percolation p = new Percolation(n);
+		int count = 0;
+		while (!p.percolates()) {
+			int x = StdRandom.uniform(n) + 1;
+			int y = StdRandom.uniform(n) + 1;
+			if (!p.isOpen(x, y)) {
+				p.open(x, y);
+				count++;
+			}
+		}
+		return count;
+	}
 
-    private static int testPercolation(int n) {
-        Percolation p = new Percolation(n);
-        int count = 0;
-        while (!p.percolates()) {
-            int x = StdRandom.uniform(n) + 1;
-            int y = StdRandom.uniform(n) + 1;
-            if (!p.isOpen(x, y)) {
-                p.open(x, y);
-                count++;
-            }
-        }
-        return count;
-    }
+	public double mean() {
+		// sample mean of percolation threshold
+		return mean;
+	}
 
-    public double mean() {
-        // sample mean of percolation threshold
-        return mean;
-    }
+	public double stddev() {
+		// sample standard deviation of percolation threshold
+		return stddev;
+	}
 
-    public double stddev() {
-        // sample standard deviation of percolation threshold
-        return stddev;
-    }
+	public double confidenceLo() {
+		// low endpoint of 95% confidence interval
+		return low;
+	}
 
-    public double confidenceLo() {
-        // low endpoint of 95% confidence interval
-        return mean - (1.96 * Math.sqrt(stddev) / n);
-    }
-
-    public double confidenceHi() {
-        // high endpoint of 95% confidence interval
-        return mean + (1.96 * Math.sqrt(stddev) / n);
-    }
+	public double confidenceHi() {
+		// high endpoint of 95% confidence interval
+		return high;
+	}
 
 }
